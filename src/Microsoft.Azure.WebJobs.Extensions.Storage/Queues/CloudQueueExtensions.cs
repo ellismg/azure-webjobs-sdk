baa48f1edcf34e63056db3466445b36ce9098493
Matt Ellis <matell@microsoft.com>
2020-01-30 12:59:27 -0800

    Implement `CreateIfNotExistsAsync` in CloudQueueExtensions
    There's a work item to add this back to the Azure Queues SDK, but until
    that is complete we can implement it ourselves.

--- src/Microsoft.Azure.WebJobs.Extensions.Storage/Queues/CloudQueueExtensions.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.Storage/Queues/CloudQueueExtensions.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Storage.Queues;
 using Azure.Storage.Queues.Models;
 
@@ -13,9 +14,17 @@ namespace Microsoft.Azure.WebJobs.Host.Queues
 {
     internal static class CloudQueueExtensions
     {
-        public static Task<bool> CreateIfNotExistsAsync(this QueueClient queue, CancellationToken cancellationToken)
+        // Adding CreateIfNotExists back to the Queue SDK is tracked with: https://github.com/Azure/azure-sdk-for-net/issues/7879
+        public static async Task CreateIfNotExistsAsync(this QueueClient queue, CancellationToken cancellationToken)
         {
-            return queue.CreateIfNotExistsAsync(null, null, cancellationToken);
+            try
+            {
+                Response r = await queue.CreateAsync(null, cancellationToken).ConfigureAwait(false);
+            }
+            catch (RequestFailedException e) when (e.ErrorCode == "QueueAlreadyExists")
+            {
+                // Queue already exists, safe to ignore.
+            }
         }
     }
 }
